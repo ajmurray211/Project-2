5278e14fd05fe0a4c4caf01d6582488141f2c497
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-05-17 13:37:59 -0400

    Recipes component Created a recipes component with placeholder data inside. Imported Recipes.js to App.js and created a route and link for the component.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Routes, Link} from 'react-router-dom'
-import {useState, useEffect} from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Home from './Components/Home';
+import Recipes from './Components/Recipes';
 
 function App() {
-const [searchVal, setSearchVal] = useState('')
+  const [searchVal, setSearchVal] = useState('')
 
-const handleChange = (event) => {
-  setSearchVal(event.target.value)
-}
+  const handleChange = (event) => {
+    setSearchVal(event.target.value)
+  }
 
-const handleSubmit = (event) => {
-  event.preventDefault()
-  setSearchVal('')
-}
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setSearchVal('')
+  }
 
   return (
     <div className="App">
       <nav>
-        <Link to='/'>Home</Link>
+        <Link to='/'> Home </Link>
+        <Link to='/recipes'> Recipes </Link>
       </nav>
 
       <div>
         <Routes>
           <Route path='/' element={<Home
-          searchVal={searchVal}
-          handleChange={handleChange}
-          handleSubmit={handleSubmit}
+            searchVal={searchVal}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
           />}></Route>
+          <Route path='/recipes' element={<Recipes></Recipes>}></Route>
         </Routes>
       </div>
     </div>
---
+++ src/Components/Recipes.js
@@ -0,0 +1,11 @@
+const Recipes = () => {
+    return ( 
+        <div>
+            <img src='#' alt="placeholder"/>
+            <section>Information about the recipes will go here</section>
+            <button>Favorite</button>
+        </div>
+     );
+}
+ 
+export default Recipes;
