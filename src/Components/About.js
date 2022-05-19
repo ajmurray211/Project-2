463301c5a5337f20796122cff60ddf47e4104b90
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-05-18 20:05:48 -0400

    Touch ups small changes to the home page to change the welcome and edits to the about page to describe the intent of the site and general directions

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,14 @@
 const About = () => {
     return ( 
         <div>
-            Information about how the site works and its intent
+            <p>This website is designed for people that love nature. This is your one stop shop for all your 
+                information currently this website is under construction. The operational portion of this site 
+                currently is the recipe portion. You can type in an animal or general recipe that you would like. 
+                For example try “pie” or “beef” and look at some recipes. You can favorite recipes that will appear 
+                at the bottom of your screen, you can click on the name you favorited and it will bring you to an 
+                in depth recipe for you to follow for that dish. Have fun and feel free to contact me if you 
+                find a bug or have a problem!
+            </p>
         </div>
      );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ const Home = (props) => {
     return (
         <div>
             <h1>Welcome</h1>
-            <h3>Description of what the page is</h3>
+            <h3>This is a one stop shop for all things hunting and fishing</h3>
         </div>
     );
 }
