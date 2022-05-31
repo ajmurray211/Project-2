9bd7ba1c5bb208362d647dd32383a0ad8ef606f4
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-05-30 22:06:57 -0400

    Added hunting/fishing data files Added files containing properly formated json to add to a data base later on. So far its just an array or objects containing names of animals with the first animal in each file holding a rough draft of what every animal will have.

---
+++ backEnd/fishingData.js
@@ -0,0 +1,170 @@
+const fishingData = [
+    // trout section
+    {
+        "name": "Brown trout",
+        "imgMain": "",
+        "imgGallery": ["", "", ""],
+        "color": " ",
+        "size": "",
+        "country": " ",
+        "region": "",
+        "diet": "",
+        "habitat": "",
+        "breedingSeason": "",
+        "typeOfWater": "Fresh",
+        "lifespan": "",
+        "endangeredRating": "",
+        "generalInfo": ["", "", ""]
+    },
+    {
+        "name": "Rainbow trout"
+    },
+    {
+        "name": "Golden trout"
+    },
+    {
+        "name": "Lake trout"
+    },
+    {
+        "name": "Tiger trout"
+    },
+    {
+        "name": "Cutthroat trout"
+    },
+    {
+        "name": "Sea trout"
+    },
+    {
+        "name": "Bull trout",
+    },
+    // Bass section
+    {
+        "name": "Largemouth bass"
+    },
+    {
+        "name": "Striped bass"
+    },
+    {
+        "name": "Black sea bass"
+    },
+    {
+        "name": "Smallmouth bass"
+    },
+    {
+        "name": "Rock bass"
+    },
+    {
+        "name": "Giant sea bass"
+    },
+    {
+        "name": "Spotted bass"
+    },
+    {
+        "name": "Yellow bass"
+    },
+    // salmon section
+    {
+        "name": "Sockeye salmon"
+    },
+    {
+        "name": "Atlantic salmon"
+    },
+    {
+        "name": "Chum salmon"
+    },
+    {
+        "name": "Chinook salmon"
+    },
+    {
+        "name": "Coho salmon"
+    },
+    {
+        "name": "Pink salmon"
+    },
+    {
+        "name": "Black sea salmon"
+    },
+    // types of sunfish
+    {
+        "name": "Spotted sunfish"
+    },
+    {
+        "name": "Dollar sunfish"
+    },
+    {
+        "name": "Orangespotted sunfish"
+    },
+    {
+        "name": "Lonegear sunfish"
+    },
+    {
+        "name": "Redbrest sunfish"
+    },
+    {
+        "name": "Green sunfish"
+    },
+    {
+        "name": "Redear sunfish"
+    },
+    {
+        "name": "Bluegill"
+    },
+    {
+        "name": "Pumpkinseed"
+    },
+    {
+        "name": "Warmouth"
+    },
+    // pike types
+    {
+        "name": "Northern pike"
+    },
+    {
+        "name": "Chain pickerel"
+    },
+    {
+        "name": "Clear muskie"
+    },
+    {
+        "name": "Gress pickerel"
+    },
+    {
+        "name": "Muskellunge"
+    },
+    {
+        "name": "Redfin pickerel"
+    },
+    {
+        "name": "Tiger Muskie"
+    },
+    // types of catfish
+    {
+        "name": "Wels catfish"
+    },
+    {
+        "name": "Blue catfish"
+    },
+    {
+        "name": "Flathead catfish"
+    },
+    {
+        "name": "Channel catfish"
+    },
+    {
+        "name": "White catfish"
+    },
+    {
+        "name": "Yellow bullhead"
+    },
+    {
+        "name": "Brown bullhead"
+    },
+    {
+        "name": "Black bullhead"
+    },
+    {
+        "name": "Headwater catfish"
+    },
+]
+
+module.exports = fishingData
---
+++ backEnd/huntingData.js
@@ -0,0 +1,321 @@
+const huntingData = [
+    {
+        // hoofed animals
+        "name": "Rocky mountain elk",
+        "imgMain": "",
+        "imgGallery": ["", "", ""],
+        "color": " ",
+        "size": "",
+        "country": " ",
+        "region": "",
+        "diet": "",
+        "habitat": "",
+        "breedingSeason": "",
+        "lifespan": "",
+        "endangeredRating": "",
+        "generalInfo": ["", "", ""]
+    },
+    {
+        "name": "Moose"
+    },
+    {
+        "name": "Pronhorn"
+    },
+    {
+        "name": "Dall sheep"
+    },
+    {
+        "name": "Whit-tailed deer"
+    },
+    {
+        "name": "Mule deer"
+    },
+    {
+        "name": "American bison"
+    },
+    {
+        "name": "Bighorn sheep"
+    },
+    {
+        "name": "Sika deer"
+    },
+    {
+        "name": "Columbia blacktail deer"
+    },
+    {
+        "name": "Coues deer"
+    },
+    {
+        "name": "Roosevelt elk"
+    },
+    {
+        "name": "Tule elk"
+    },
+    {
+        "name": "Barren ground caribou"
+    },
+    {
+        "name": "Mountain caribou"
+    },
+    {
+        "name": "Woodland caribou"
+    },
+    {
+        "name": "Alaska youkon moose"
+    },
+    {
+        "name": "Shiras moose"
+    },
+    {
+        "name": "Muskox"
+    },
+    {
+        "name": "American mountain goat"
+    },
+    {
+        "name": "Pronghorn antelope"
+    },
+    {
+        "name": "California bighorn sheep"
+    },
+    {
+        "name": "Desert bighorn sheep"
+    },
+    {
+        "name": "Rockey mountain bighorn sheep"
+    },
+    {
+        "name": "Stone sheep"
+    },
+    {
+        "name": "Hog"
+    },
+    // Bears
+    {
+        "name": "Alaska brown bear"
+    },
+    {
+        "name": "Black bear"
+    },
+    {
+        "name": "Grizzly bear"
+    },
+    // Big Cats
+    {
+        "name": "Cougar"
+    },
+    {
+        "name": "Bobcat"
+    },
+    // Squirrles
+    {
+        "name": "Eastern grey squirrel"
+    },
+    {
+        "name": "Fox squirrel"
+    },
+    {
+        "name": "Western grey squirrel"
+    },
+    {
+        "name": "American red Squirrel"
+    },
+    {
+        "name": "Aberts squirrel"
+    },
+    {
+        "name": "Arizona grey squirrel"
+    },
+    //cotton tails 
+    {
+        "name": "Swamp rabbit"
+    },
+    {
+        "name": "Desert cottontail"
+    },
+    {
+        "name": "Eastern cottontail"
+    },
+    {
+        "name": "Mountain cottontail"
+    },
+    {
+        "name": "Marsh rabbit"
+    },
+    {
+        "name": "New England cottontail"
+    },
+    // coyotes
+    {
+        "name": "Western coyotes"
+    },
+    {
+        "name": "Centeral U.S. coyotes"
+    },
+    {
+        "name": "Eastern coyotes"
+    },
+    //various small game 
+    {
+        "name": "American beaver"
+    },
+    {
+        "name": "Long tailed weasel"
+    },
+    {
+        "name": "Muskrat"
+    },
+    {
+        "name": "Mink"
+    },
+    {
+        "name": "Nutria"
+    },
+    {
+        "name": "Groundhog"
+    },
+    {
+        "name": "River otter"
+    },
+    {
+        "name": "Raccoon"
+    },
+    {
+        "name": "Opossum"
+    },
+    {
+        "name": "Skunk"
+    },
+    {
+        "name": "Fox"
+    },
+    //Water foul
+    //geese
+    {
+        "name": "Canada goose"
+    },
+    {
+        "name": "Snow goose"
+    },
+    {
+        "name": "White-fronted goose"
+    },
+    {
+        "name": "Ross goose"
+    },
+    {
+        "name": "Atlantic brant"
+    },
+    {
+        "name": "Pacific brant"
+    },
+    {
+        "name": "Emperor goose"
+    },
+    {
+        "name": "Barnacle goose"
+    },
+    {
+        "name": "Cackling goose"
+    },
+    //Ducks
+    {
+        "name": "American black duck"
+    },
+    {
+        "name": "American wigeon"
+    },
+    {
+        "name": "Blue-winged teal"
+    },
+    {
+        "name": "Cinnamon teal"
+    },
+    {
+        "name": "Eurasian wigeon"
+    },
+    {
+        "name": "Gladwall"
+    },
+    {
+        "name": "Green-winged teal"
+    },
+    {
+        "name": "Mallard"
+    },
+    {
+        "name": "Mottled duck"
+    },
+    {
+        "name": "Northern pintail"
+    },
+    {
+        "name": "Northern shoveler"
+    },
+    {
+        "name": "Wood duck"
+    },
+    {
+        "name": "Harlequin duck"
+    },
+    {
+        "name": "Long-tailed duck"
+    },
+    {
+        "name": "Ring-necked duck"
+    },
+    //diving birds
+    {
+        "name": "Barrows goldeneye"
+    },
+    {
+        "name": "Black scoter"
+    },
+    {
+        "name": "Bufflehead"
+    },
+    {
+        "name": "Canvasback"
+    },
+    {
+        "name": "Common eider"
+    },
+    {
+        "name": "Common goldeneye"
+    },
+    {
+        "name": "Common merganser"
+    },
+    {
+        "name": "Greater scaup"
+    },
+    {
+        "name": "Hooded merganser"
+    },
+    {
+        "name": "King eider"
+    },
+    {
+        "name": "Lesser scaup"
+    },
+    {
+        "name": "Red-brested merganser"
+    },
+    {
+        "name": "Redhead"
+    },
+    {
+        "name": "Surf scoter"
+    },
+    {
+        "name": "White-winged scoter"
+    },
+    {
+        "name": "Specaled eider"
+    },
+    {
+        "name": "Stellers eider"
+    },
+]
+
+module.exports = huntingData
