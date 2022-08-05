bfddb06d0aa8e28b992c8d2ef644c1ae50c70a09
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-08-05 10:58:59 -0400

    Version one of fishingtile with expanded information popup

--- src/Components/FishTile.js
+++ src/Components/FishTile.js
@@ -1,9 +1,36 @@
-const FishTile = (porps) => {
+import { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter} from "reactstrap";
+
+const FishTile = (props) => {
+
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+
     return (
-        <li className="Tile">
-            <img src={porps.fish.imgMain} alt={porps.fish.name}/>
-            <p>Name: {porps.fish.name}</p>
-        </li>
+        <>
+            <li className="Tile">
+                <img src={props.fish.imgMain} alt={props.fish.name} />
+                <p>Name: {props.fish.name}</p>
+                <Button onClick={toggle}>More information</Button>
+            </li>
+
+            <Modal isOpen={modal} toggle={toggle} centered={true} fullscreen="xl" size="xl">
+                <ModalHeader toggle={toggle}>{props.fish.name}</ModalHeader>
+                <ModalBody>
+                    <p>Diet: {props.fish.diet}</p>
+                    <p>Family: {props.fish.family}</p>
+                    <p>Habitat: {props.fish.habitat}</p>
+                    <p>Type of water: {props.fish.typeOfWater}</p>
+                    <p>Breeding season: {props.fish.breedingSeason}</p>
+                    <p>Average weight: {props.fish.avgWeight}</p>
+                    <p>Average length: {props.fish.avgLength}</p>
+                    <p>Average lifespan: {props.fish.avgLifespan}</p>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="secondary" onClick={toggle}>Close</Button>
+                </ModalFooter>
+            </Modal>
+        </>
     );
 }
 
