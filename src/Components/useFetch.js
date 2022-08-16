a71a883aefe079e45fdccf15477825e0075351f7
AJ Murray <aj.murr4y@gmail.com>
2022-08-16 15:39:13 -0400

    added a refetch function to act as a reset option

--- src/Components/useFetch.js
+++ src/Components/useFetch.js
@@ -2,13 +2,14 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function useFetch(url) {
-    const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
     useEffect(() => {
         setLoading(true)
-        axios.get(url)
+        axios
+            .get(url)
             .then((response) => {
                 setData(response.data)
             })
@@ -20,8 +21,22 @@ function useFetch(url) {
             })
     }, [url])
 
-    return{data, loading, error}
+    const refetch = () => {
+        setLoading(true)
+        axios
+            .get(url)
+            .then((response) => {
+                setData(response.data)
+            })
+            .catch((err) => {
+                setError(err)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }
 
+    return { data, loading, error, refetch }
 }
 
 export default useFetch;
