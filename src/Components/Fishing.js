5696ebfd49fe45e73a7f72ae2720a0f0ba42f31e
AJ Murray <aj.murr4y@gmail.com>
2022-08-17 17:19:15 -0400

    restructured the dropdown layouts

--- src/Components/Fishing.js
+++ src/Components/Fishing.js
@@ -1,15 +1,14 @@
 import { useState, useEffect, useReducer } from 'react'
 import FishTile from './FishTile'
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, Spinner } from 'reactstrap';
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, Spinner } from 'reactstrap';
 import useFetch from './useFetch';
 import axios from 'axios'
 
 
-// const API_URL = 'https://quiet-gorge-88095.herokuapp.com'
-const API_URL = 'http://127.0.0.1:8000'
+const API_URL = 'https://quiet-gorge-88095.herokuapp.com'
+// const API_URL = 'http://127.0.0.1:8000'
 
 const Fishing = () => {
-    const [fishData, setFishData] = useState([])
     const [activeFishSearch, setActiveFishSearch] = useState('')
     const [activeSearchBy, setActiveSearchBy] = useState('')
     const [data, setData] = useState([])
@@ -20,6 +19,7 @@ const Fishing = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const toggle = (event) => {
         event.preventDefault()
+        console.log(event.target)
         setDropdownOpen(prevState => !prevState);
     }
 
@@ -51,7 +51,6 @@ const Fishing = () => {
         event.preventDefault()
         setActiveFishSearch(event.target.name)
         setActiveSearchBy(event.target.value)
-        console.log(event.target.name, event.target.value)
     }
 
     // mapping over the state and returning a tile
@@ -63,25 +62,31 @@ const Fishing = () => {
 
     return (
         <>
-            <Dropdown isOpen={dropdownOpen} toggle={toggle} >
-                <Button size='sm' onClick={() => {
+            <div className="d-flex p-5 justify-content-center">
+                <Button color='dark' className='me-2' onClick={() => {
                     setActiveFishSearch('')
-                }}>Reset</Button>
-                <DropdownToggle caret size='sm'>
-                    Family
-                </DropdownToggle>
-                <DropdownMenu>
-                    <DropdownItem name='Salmon' value='family' onClick={filterFish}>Salmon</DropdownItem>
-                    <DropdownItem name='Sunfish' value='family' onClick={filterFish}>Sunfish</DropdownItem>
-                    <DropdownItem name='Pike' value='family' onClick={filterFish}>Pike</DropdownItem>
-                    <DropdownItem name='Catfish' value='family' onClick={filterFish}>Catfish</DropdownItem>
-                </DropdownMenu>
-                <DropdownToggle caret size='sm'> Water type </DropdownToggle>
-                <DropdownMenu>
-                    <DropdownItem name='Fresh' value='typeOfWater' onClick={filterFish}>Fresh</DropdownItem>
-                    <DropdownItem name='Salt' value='typeOfWater' onClick={filterFish}>Salt</DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
+                }}> Reset </Button>
+                <UncontrolledDropdown
+                    className="me-2"
+                >
+                    <DropdownToggle color='dark' caret>Family</DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem name='Salmon' value='family' onClick={filterFish} > Salmon </DropdownItem>
+                        <DropdownItem name='Sunfish' value='family' onClick={filterFish} > Sunfish </DropdownItem>
+                        <DropdownItem name='Pike' value='family' onClick={filterFish} > Pike </DropdownItem>
+                        <DropdownItem name='Catfish' value='family' onClick={filterFish} > Catfish </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+
+                <UncontrolledDropdown className="me-2" >
+                    <DropdownToggle color='dark' caret> Water type</DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem name='Fresh' value='typeOfWater' onClick={filterFish}> Fresh </DropdownItem>
+                        <DropdownItem name='Salt' value='typeOfWater' onClick={filterFish}> Salt </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+
+            </div>
 
             <ul className='container'>
                 {loading ? <Spinner animation="border" /> : mappedFish}
