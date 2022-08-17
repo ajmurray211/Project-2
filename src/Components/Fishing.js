a70612aa31b914f8fd042e2c0d484ebd1b2556b6
AJ Murray <aj.murr4y@gmail.com>
2022-08-17 15:26:57 -0400

    restructered the api call to fit back end search params, added a dev api url and a production api url, added an axios promises to handle loading data for conditional spinner rendering.

--- src/Components/Fishing.js
+++ src/Components/Fishing.js
@@ -1,76 +1,90 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer } from 'react'
 import FishTile from './FishTile'
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Button } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, Spinner } from 'reactstrap';
+import useFetch from './useFetch';
+import axios from 'axios'
+
+
+// const API_URL = 'https://quiet-gorge-88095.herokuapp.com'
+const API_URL = 'http://127.0.0.1:8000'
 
 const Fishing = () => {
     const [fishData, setFishData] = useState([])
-    const [reset, setReset] = useState(true)
-    const [activeFishFamily, setActiveFishFamily] = useState('')
+    const [activeFishSearch, setActiveFishSearch] = useState('')
+    const [activeSearchBy, setActiveSearchBy] = useState('')
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     //controls the filter toggling
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const toggle = (event) => {
         event.preventDefault()
-        // console.log(event)
         setDropdownOpen(prevState => !prevState);
     }
 
-    // endpoint for personal api
-    // https://quiet-gorge-88095.herokuapp.com/fish/?format=json
-    // this function makes the api call for all of the fish
-    const getData = () => {
-        fetch(`https://quiet-gorge-88095.herokuapp.com/fish/?format=json`)
-            .then(response => response.json())
-            .then(data => setFishData(data))
-            // .then(console.log('api call made'))
-            .catch(() => console.log('Call failed'))
+    const loadData = (url) => {
+        setLoading(true)
+        axios
+            .get(url)
+            .then((response) => {
+                setData(response.data)
+            })
+            .catch((err) => {
+                setError(err)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
     }
 
-    //Makes the api call for all the fish on filter reset of page load
     useEffect(() => {
-        getData()
-    }, [reset])
+        if (activeFishSearch === '') {
+            loadData(`${API_URL}/fish/?format=json`)
+        } else {
+            loadData(`${API_URL}/fish/?format=json&${activeSearchBy}=${activeFishSearch}`)
+        }
+    }, [activeFishSearch])
 
     //filters the fish based on user selection
     const filterFish = (event) => {
         event.preventDefault()
-        const copyData = [...fishData]
-        const filtered = copyData.filter(fish => event.target.name === fish.family)
-        setFishData(filtered)
-    }
-
-    //handles the reset filter button
-    const handleReset = (event) => {
-        event.preventDefault()
-        getData()
+        setActiveFishSearch(event.target.name)
+        setActiveSearchBy(event.target.value)
+        console.log(event.target.name, event.target.value)
     }
 
     // mapping over the state and returning a tile
-    const mappedFish = fishData.map((fish) => {
-        if (fish) {
-            return <FishTile
-                fish={fish}
-            />
-        }
+    const mappedFish = data.map((fish) => {
+        return <FishTile
+            fish={fish}
+        />
     })
 
     return (
         <>
             <Dropdown isOpen={dropdownOpen} toggle={toggle} >
-                <Button size='sm' onClick={handleReset}>Reset</Button>
+                <Button size='sm' onClick={() => {
+                    setActiveFishSearch('')
+                }}>Reset</Button>
                 <DropdownToggle caret size='sm'>
                     Family
                 </DropdownToggle>
                 <DropdownMenu>
-                    <DropdownItem name='Salmon' onClick={filterFish}>Salmon</DropdownItem>
-                    <DropdownItem name='Sunfish' onClick={filterFish}>Sunfish</DropdownItem>
-                    <DropdownItem name='Pike' onClick={filterFish}>Pike</DropdownItem>
-                    <DropdownItem name='Catfish' onClick={filterFish}>Catfish</DropdownItem>
+                    <DropdownItem name='Salmon' value='family' onClick={filterFish}>Salmon</DropdownItem>
+                    <DropdownItem name='Sunfish' value='family' onClick={filterFish}>Sunfish</DropdownItem>
+                    <DropdownItem name='Pike' value='family' onClick={filterFish}>Pike</DropdownItem>
+                    <DropdownItem name='Catfish' value='family' onClick={filterFish}>Catfish</DropdownItem>
+                </DropdownMenu>
+                <DropdownToggle caret size='sm'> Water type </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem name='Fresh' value='typeOfWater' onClick={filterFish}>Fresh</DropdownItem>
+                    <DropdownItem name='Salt' value='typeOfWater' onClick={filterFish}>Salt</DropdownItem>
                 </DropdownMenu>
             </Dropdown>
 
             <ul className='container'>
-                {mappedFish}
+                {loading ? <Spinner animation="border" /> : mappedFish}
             </ul>
         </>
     );
